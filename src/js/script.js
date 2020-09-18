03553d4b34f52110de02d1abef59c8bad14316e5
Magdalena Osik <magdalenaosik@gmail.com>
2020-09-18 21:57:04 +0200

    dd new files

--- src/js/script.js
+++ src/js/script.js
@@ -249,180 +249,230 @@ const { get } = require("browser-sync");
           }
         }
       }
-    
-
-  /* multiply price by amount */
-  thisProduct.priceSingle = price;
-  thisProduct.price = thisProduct.priceSingle * thisProduct.amountWidget.value;
-
-  /* set the contents of thisProduct.priceElem to be the value of variable price */
-  thisProduct.priceElem.innerHTML = thisProduct.price;
-  }
 
 
-initAmountWidget() {
-  const thisProduct = this;
-  thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
-  thisProduct.amountWidgetElem.addEventListener('updated', function () {
-    thisProduct.processOrder();
-  });
-}
+      /* multiply price by amount */
+      thisProduct.priceSingle = price;
+      thisProduct.price = thisProduct.priceSingle * thisProduct.amountWidget.value;
 
-addTooCart() {
-  const thisProduct = this;
-  thisProduct.name = thisProduct.data.name;
-  thisProduct.amount = thisProduct.amountWidget.value;
-  app.cart.add(thisProduct);
-
-}
-  }
-
-class AmountWidget {
-  constructor(element) {
-    const thisWidget = this;
-    thisWidget.getElements(element);
-
-    console.log('AmountWidget:', thisWidget);
-    console.log('constructor arguments:', element);
-  }
+      /* set the contents of thisProduct.priceElem to be the value of variable price */
+      thisProduct.priceElem.innerHTML = thisProduct.price;
+    }
 
 
-  getElements(element) {
-    const thisWidget = this;
+    initAmountWidget() {
+      const thisProduct = this;
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+      thisProduct.amountWidgetElem.addEventListener('updated', function () {
+        thisProduct.processOrder();
+      });
+    }
 
-    thisWidget.element = element;
-    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
-  }
+    addTooCart() {
+      const thisProduct = this;
+      thisProduct.name = thisProduct.data.name;
+      thisProduct.amount = thisProduct.amountWidget.value;
+      app.cart.add(thisProduct);
 
-
-  setValue(value) {
-    const thisWidget = this;
-    const newValue = parseInt(value);
-
-    /* To do : Add validation*/
-    thisWidget.value = newValue;
-    thisWidget.input.value = thisWidget.value;
-    thisWidget.setValue(thisWidget.input.value);
-
-  }
-
-  announce() {
-    const thisWidget = this;
-
-    const event = new CustomEvent('updated', {
-      bubbles: true
-    });
-    thisWidget.dom.wrapper.dispatchEvent(event);
-  }
-
-  initActions() {
-    const thisWidget = this;
-    thisWidget.dom.linkIncrease.addEventListener('click', function (event) {
-      event.preventDefault();
-      thisWidget.setValue(thisWidget.value + 1);
-    });
-
-    thisWidget.dom.linkDecrease.addEventListener('click', function (event) {
-      event.preventDefault();
-      thisWidget.setValue(thisWidget.value - 1);
-    });
-
-    thisWidget.dom.input.addEventListener('change', function () {
-      thisWidget.value = thisWidget.dom.input.value;
-    });
-  }
-}
-
-class Cart {
-  constructor(element) {
-    const thisCart = this;
-    thisCart.product = [];
-    thisCart.deliveryFee = settings.cart.defaultDeliveryFee;
-    console.log('new Cart', thisCart);
-  }
-  getElements(element) {
-    const thisCart = this;
-    thisCart.dom = {};
-    thisCart.dom.wrapper = element;
-    thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
-    thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
-
-    thisCart.renderTotalsKeys = ['totalNumber', 'totalPrice', 'subtotalPrice', 'deliveryFee'];
-
-    for (let key of thisCart.renderTotalsKeys) {
-      thisCart.dom[key] = thisCart.dom.wrapper.querySelectorAll(select.cart[key]);
     }
   }
 
+  class AmountWidget {
+    constructor(element) {
+      const thisWidget = this;
+      thisWidget.getElements(element);
 
-
-  initActions() {
-    const thisCart = this;
-    thisCart.dom.toggleTrigger.addEventListener('click', function (event) {
-      event.preventDefault();
-    });
-    thisCart.dom.toggleTrigger.addEventListener('click', function () {
-      thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
-    });
-    thisCart.dom.productList.addEventListener('updated', function () {
-      thisCart.update();
-    });
-  }
-
-  add(menuProduct) {
-    const thisCart = this;
-    /*generate HTML based on temple w scripcie o id="template-cart-product" przekazujemy cały obiekt menuProduct */
-    const generatedHTML = templates.cartProduct(menuProduct);
-
-    /*create element using utils.createElementFromHTML powyższy kod zamieniamy na elementy DOM */
-    const generatedDOM = utils.createDOMFromHTML(generatedHTML);
-
-    /*find cart container -  dodajemy te elementy do DOM*/
-    const cartContainer = document.querySelector(thisCart.dom.productList);
-    /*add element to menu*/
-    cartContainer.appendChild(menuProduct);
-    console.log('adding product', menuProduct);
-
-    thisCart.product.push(new CartProduct(menuProduct, generatedDOM));
-    console.log('thisCart', thisCart.products);
-  }
-
-
-  update() {
-    const thisCart = this;
-    thisCart.totalNumber = 0;
-    thisCart.subtotalPrice = 0;
-    for (let product of thisCart.products) {
-      thisCart.subtotalPrice += product.price;
-      thisCart.totalNumber += product.amount;
+      console.log('AmountWidget:', thisWidget);
+      console.log('constructor arguments:', element);
     }
-    thisCart.totalPrice = thisCart.subtotalPrice + thisCart.deliveryFee;
 
-    console.log(totalNumber);
-    console.log(subtotalPrice);
-    console.log(thisCart.totalPrice);
 
-    for (let key of thisCart.renderTotalsKeys) {
-      for (let elem of thisCart.dom[key]) {
-        elem.innerHTML = thisCart[key];
+    getElements(element) {
+      const thisWidget = this;
+
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+
+
+    setValue(value) {
+      const thisWidget = this;
+      const newValue = parseInt(value);
+
+      /* To do : Add validation*/
+      thisWidget.value = newValue;
+      thisWidget.input.value = thisWidget.value;
+      thisWidget.setValue(thisWidget.input.value);
+
+    }
+
+    announce() {
+      const thisWidget = this;
+
+      const event = new CustomEvent('updated', {
+        bubbles: true
+      });
+      thisWidget.dom.wrapper.dispatchEvent(event);
+    }
+
+    initActions() {
+      const thisWidget = this;
+      thisWidget.dom.linkIncrease.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value + 1);
+      });
+
+      thisWidget.dom.linkDecrease.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value - 1);
+      });
+
+      thisWidget.dom.input.addEventListener('change', function () {
+        thisWidget.value = thisWidget.dom.input.value;
+      });
+    }
+  }
+
+  class Cart {
+    constructor(element) {
+      const thisCart = this;
+      thisCart.product = [];
+      thisCart.deliveryFee = settings.cart.defaultDeliveryFee;
+      console.log('new Cart', thisCart);
+    }
+    getElements(element) {
+      const thisCart = this;
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+      thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
+      thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
+      thisCart.dom.address = thisCart.wrapper.querySelector(select.cart.address);
+      thisCart.dom.phone = thisCart.wrapper.querySelector(select.cart.phone);
+
+
+      for (let key of thisCart.renderTotalsKeys) {
+        thisCart.dom[key] = thisCart.dom.wrapper.querySelectorAll(select.cart[key]);
       }
     }
 
+
+
+    initActions() {
+      const thisCart = this;
+      thisCart.dom.toggleTrigger.addEventListener('click', function (event) {
+        event.preventDefault();
+      });
+      thisCart.dom.toggleTrigger.addEventListener('click', function () {
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+      thisCart.dom.productList.addEventListener('updated', function () {
+        thisCart.update();
+      });
+      thisCart.dom.form.addEventListener('submit', function () {
+        event.preventDefault();
+        thisCart.sendOrder();
+      });
+    }
+
+    add(menuProduct) {
+      const thisCart = this;
+      /*generate HTML based on temple w scripcie o id="template-cart-product" przekazujemy cały obiekt menuProduct */
+      const generatedHTML = templates.cartProduct(menuProduct);
+
+      /*create element using utils.createElementFromHTML powyższy kod zamieniamy na elementy DOM */
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+
+      /*find cart container -  dodajemy te elementy do DOM*/
+      const cartContainer = document.querySelector(thisCart.dom.productList);
+      /*add element to menu*/
+      cartContainer.appendChild(menuProduct);
+      console.log('adding product', menuProduct);
+
+      thisCart.product.push(new CartProduct(menuProduct, generatedDOM));
+      console.log('thisCart', thisCart.products);
+    }
+
+
+    update() {
+      const thisCart = this;
+      thisCart.totalNumber = 0;
+      thisCart.subtotalPrice = 0;
+      for (let product of thisCart.products) {
+        thisCart.subtotalPrice += product.price;
+        thisCart.totalNumber += product.amount;
+      }
+      thisCart.totalPrice = thisCart.subtotalPrice + thisCart.deliveryFee;
+
+      console.log(totalNumber);
+      console.log(subtotalPrice);
+      console.log(thisCart.totalPrice);
+
+      for (let key of thisCart.renderTotalsKeys) {
+        for (let elem of thisCart.dom[key]) {
+          elem.innerHTML = thisCart[key];
+        }
+      }
+
+    }
+    remove() {
+      const thisCart = this;
+      /* stała index, której wartością jest index cartProduct w tablicy thisCart.products*/
+      const index = thisProduct.prducts.indexOf(cartProduct);
+      /* użyć metody splice ( przymuje ona 2 argumenty:indeks pierwszego usuwanego elementu oraz liczbę elementów,
+       licząc od pierwszego usuwanego elementu. )
+       do usunięcia elementu  o tym indeksie z tablicy thisCart.products ?ale ile elementów usuwamy może 1 bo jest jeden element w tablicy??*/
+      thisCart.products.splice(index, 1);
+      /* usunąć z DOM element cartProduct.dom.wrapper- wpisać element DOM do usunięcia funkcję remove i pusty nawias*/
+      thisCart.cartProduct.dom.wrapper.remove();
+      thisCart.update();
+    }
+
+    sendOrder() {
+      /* w stałej dodajemy adres endpoitu  Order*/
+      const url = settings.db.url + '/' + settings.db.order;
+      /* deklaruję stałą ładunek - tzw dane z serwera*/
+      const payload = {
+        address: 'test',
+        /*zapisuję wartości zaliczne do update - do aktualizacji danych w zamówieniu*/
+        totalPrice: thisCart.totalPrice,
+        totalNumber: thisCart.totalNumber,
+        subtotalPrice: thisCart.subtotalPrice,
+        deliveryFee: thisCart.deliveryFee,
+        product: [],
+      };
+/* dodaję pętlę iterującą po wszystkich thisCart.products,*/
+ for (let product of thisCart.products ) {
+   /*i dla każdego produktu wywołam jego metodę getData*//*Wynik zwracany przez tą metodą dodaj do tablicy payload.products.*/
+   payload.products.return(product.getData() );
+   
+ }
+
+      /*konfiguruję zapytanie tu jest POST czyli wysyłania danych do API*/
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      };
+    }
   }
-  remove() {
-    const thisCart = this;
-    /* stała index, której wartością jest index cartProduct w tablicy thisCart.products*/
-    const index = thisProduct.prducts.indexOf(cartProduct);
-    /* użyć metody splice ( przymuje ona 2 argumenty:indeks pierwszego usuwanego elementu oraz liczbę elementów,
-     licząc od pierwszego usuwanego elementu. )
-     do usunięcia elementu  o tym indeksie z tablicy thisCart.products ?ale ile elementów usuwamy może 1 bo jest jeden element w tablicy??*/
-    thisCart.products.splice(index, 1);
-    /* usunąć z DOM element cartProduct.dom.wrapper- wpisać element DOM do usunięcia funkcję remove i pusty nawias*/
-    thisCart.cartProduct.dom.wrapper.remove();
-    thisCart.update();
-  }
+
+  fetch(url, options)
+    /* chainnig czyli łączenie kilku metod ze sobą za pomocą kropki*/
+    .then(function (response) {
+      /* otrzymaną odpowiedź konwertujemy z JSONa na tablicę*/
+      return response.json();
+    })
+    .then(function (parsedResponse) {
+      /* po otrzymaniu skorwentowanej odpowiedzi parsedResponse wyswietlamy w konsoli*/
+      console.log('parsedResponse', parsedResponse);
+    });
+
+  console.log('thisApp.data', JSON.stringify(thisApp.data));
 
 }
 
@@ -491,6 +541,19 @@ class CartProduct {
       event.preventDefault();
       thisCartProduct.remove();
     });
+  }
+  /* metoda,która zwraca wszystkie informacje z CartProduct o zamawianym produkcie */
+  getData() {
+    const thisCartProduct = this;
+    const data = {
+      id: thisCartProduct.name,
+      price: thisCartProduct.price,
+      priceSingle: thisCartProduct.priceSingle,
+      amount: thisCartProduct.amount,
+      params: thisCartProduct.params
+    };
+    return data;
+  }
   }
 }
 
